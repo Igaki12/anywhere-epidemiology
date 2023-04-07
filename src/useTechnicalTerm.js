90d0e2191a600ce55518fc5787e50cf02afb44ef
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-04-07 09:32:25 +0900

    first commit

---
+++ src/useTechnicalTerm.js
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+
+export const useTechnicalTerm = () => {
+  const [technicalTerm, setTechnicalTerm] = useState([
+    // term[0]が代表名として大きく表示される。
+    {
+      term: [''],
+      explanation: '',
+    },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+    // {
+    //   term:[''],
+    //   explanation:'',
+    // },
+  ])
+  const showTechnicalTerm = () => {
+    return technicalTerm.filter(
+      (value) => value.term && value.term !== [] && value.term !== [''],
+    )
+  }
+  return {
+    showTechnicalTerm,
+  }
+}
